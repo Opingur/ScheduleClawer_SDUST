b32d651b627f8bfa906639c45b164ff3e23b52f0
Opingur <224799798+Opingur@users.noreply.github.com>
2026-09-05 14:59:03 +0800

    fix: load web exporter assets from Pages

--- web/app.js
+++ web/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const SCRIPT_URL = 'https://raw.githubusercontent.com/Opingur/ScheduleClawer_SDUST/aed5f14d3a730cc5d6272aee832ed3f6e500b9df/web/exporter-bookmarklet.js';
+  const SCRIPT_URL = 'https://opingur.github.io/ScheduleClawer_SDUST/exporter-bookmarklet.js?v=20260905-1';
   const weekInput = document.querySelector('#week-count');
   const dateInput = document.querySelector('#term-start');
   const bookmark = document.querySelector('#bookmarklet');
